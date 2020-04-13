0570e7333b9568583444a88d3c4b66c409cbdb1f
Yaroslav Denisyk <denusyk3@gmail.com>
2020-04-13 17:57:48 +0300

    change lab4.py

--- lab4.py
+++ lab4.py
@@ -1,39 +1,7 @@
 import random
 import numpy.linalg as l
 import copy
-
-
-G_table = (
-    (9985, 9750, 9392, 9057, 8772, 8534, 8332, 8159, 8010, 7880),
-    (9669, 8709, 7977, 7457, 7071, 6771, 6530, 6333, 6167, 6025),
-    (9065, 7679, 6841, 6287, 5892, 5598, 5365, 5175, 5017, 4884),
-    (8412, 6838, 5981, 5440, 5063, 4783, 4564, 4387, 4241, 4118),
-    (7808, 6161, 5321, 4803, 4447, 4184, 3980, 3817, 3682, 3568),
-    (7271, 5612, 4800, 4307, 3974, 3726, 3535, 3384, 3259, 3154),
-    (6798, 5157, 4377, 3910, 3595, 3362, 3185, 3043, 2926, 2829),
-    (6385, 4775, 4027, 3584, 3286, 3067, 2901, 2768, 2659, 2568),
-    (6020, 4450, 3733, 3311, 3029, 2823, 2666, 2541, 2439, 2353))
-
-t_table = [12.71, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365, 2.306, 2.262, 2.228, 2.201, 2.179, 2.16, 2.145, 2.131,
-           2.12, 2.11]
-
-F_table = (
-     (164.4, 199.5, 215.7, 224.6, 230.2, 234),
-     (18.5, 19.2, 19.2, 19.3, 19.3, 19.3),
-     (10.1, 9.6, 9.3, 9.1, 9, 8.9),
-     (7.7, 6.9, 6.6, 6.4, 6.3, 6.2),
-     (6.6, 5.8, 5.4, 5.2, 5.1, 5),
-     (6, 5.1, 4.8, 4.5, 4.4, 4.3),
-     (5.5, 4.7, 4.4, 4.1, 4, 3.9),
-     (5.3, 4.5, 4.1, 3.8, 3.7, 3.6),
-     (5.1, 4.3, 3.9, 3.6, 3.5, 3.4),
-     (5, 4.1, 3.7, 3.5, 3.3, 3.2),
-     (4.8, 4, 3.6, 3.4, 3.2, 3.1),
-     (4.8, 3.9, 3.5, 3.3, 3.1, 3),
-     (4.7, 3.8, 3.4, 3.2, 3.0, 2.9, 2.6, 2.4, 2.2),
-     (4.6, 3.7, 3.3, 3.1, 3.0, 2.9, 2.5, 2.3, 2.1),
-     (4.5, 3.7, 3.3, 3.1, 2.9, 2.8, 2.5, 2.3, 2.1),
-     (4.5, 3.6, 3.2, 3.0, 2.9, 2.7, 2.4, 2.2, 2.0))
+from scipy.stats import f, t
 
 #variant 110
 #           min    max
@@ -42,6 +10,16 @@ F_table = (
 #       x3   15    35
 
 
+def cohren(f1, f2, q=0.05):
+    q1 = q / f1
+    cohren1 = f.ppf(q=1 - q1, dfn=f2, dfd=(f1 - 1) * f2)
+    return round(cohren1/ (cohren1 + f1 - 1),4)
+def fisher(f1,f2, q=1-0.05):
+    fisher1=f.ppf(q,dfn=f2,dfd=f1)
+    return round(fisher1,4)
+def student(f3, q=1-0.025):
+    student1=t.ppf(q,df=f3)
+    return round(student1,4)
 def print_arr(arr):
     for i in arr: print(i)
 
@@ -64,7 +42,7 @@ x1 = [-20, 15]
 x2 = [10, 60]
 x3 = [15, 35]
 
-m = 3
+m = 2
 N = 8
 q = 0.05
 
@@ -111,50 +89,52 @@ while True:
     print("D:"+str(D))
 
     Gp = max(D) / sum(D)
-    Gt = G_table[f2 - 2][f1 - 1] * 0.0001
+    Gt = cohren(f1+1,f2)
 
     print("Однорідність дисперсії (критерій Кохрена): ")
     print("Gp = {}".format(round(Gp,5)))
     print("Gt = {}".format(round(Gt,5)))
     if Gp < Gt:
         print("Дисперсія однорідна (Gp < Gt)")
-        break
+        S = sum(D) / N
+        Sb = S / (N * m)
+        beta = [(y_regr[0] + y_regr[1] + y_regr[2] + y_regr[3] + y_regr[4] + y_regr[5] + y_regr[6] + y_regr[7]) / N,
+                (-y_regr[0] - y_regr[1] - y_regr[2] - y_regr[3] + y_regr[4] + y_regr[5] + y_regr[6] + y_regr[7]) / N,
+                (-y_regr[0] - y_regr[1] + y_regr[2] + y_regr[3] - y_regr[4] - y_regr[5] + y_regr[6] + y_regr[7]) / N,
+                (-y_regr[0] + y_regr[1] - y_regr[2] + y_regr[3] - y_regr[4] + y_regr[5] - y_regr[6] + y_regr[7]) / N,
+                (y_regr[0] + y_regr[1] - y_regr[2] - y_regr[3] - y_regr[4] - y_regr[5] + y_regr[6] + y_regr[7]) / N,
+                (y_regr[0] - y_regr[1] + y_regr[2] - y_regr[3] - y_regr[4] + y_regr[5] - y_regr[6] + y_regr[7]) / N,
+                (y_regr[0] - y_regr[1] - y_regr[2] + y_regr[3] + y_regr[4] - y_regr[5] - y_regr[6] + y_regr[7]) / N,
+                (-y_regr[0] + y_regr[1] + y_regr[2] - y_regr[3] + y_regr[4] - y_regr[5] - y_regr[6] + y_regr[7]) / N]
+        t1 = [abs(i) / Sb for i in beta]
+        t_kr = student(f3)
+        print(t_kr)
+        print(t1)
+        t_final = list(filter(lambda x: x > t_kr, t1))
+        f4 = N - len(t_final)
+        print("\nЗначимі коефіцієнти: "), print_arr(t_final)
+        print("t: " + str(t1))
+        print("t фінальна: " + str(t_final))
+        print("Бета: " + str(beta))
+
+        f_sum = 0
+        for i in range(N):
+            f_sum += pow((t1[i] - y_regr[i]), 2)
+        D_ad = (m / (f4)) * f_sum
+        print(f_sum)
+        print(D_ad)
+        print(Sb)
+        Fp = D_ad / Sb
+        print("Fp = {}".format(round(Fp, 5)))
+        Ft = fisher(f3, f4)
+        print("Ft = {}".format(round(Ft, 5)))
+        if Ft > Fp:
+            print(f"Ft > Fp\nРівняння регресії адекватно оригіналу при рівні значимості {q}")
+            break
+        else:
+            print(f"Ft < Fp\nРівняння регресії неадекватно оригіналу при рівні значимості {q}")
+            m+=1
     else:
         print("Дисперсія неоднорідна (Gp > Gt), збільшуємо m, повторюємо операції")
         m += 1
-
-S= sum(D) / N
-Sb = S/ (N * m)
-beta = [(y_regr[0] + y_regr[1] + y_regr[2] + y_regr[3] + y_regr[4] + y_regr[5] + y_regr[6] + y_regr[7]) / N,
-        (-y_regr[0] - y_regr[1] - y_regr[2] - y_regr[3] + y_regr[4] + y_regr[5] + y_regr[6] + y_regr[7]) / N,
-        (-y_regr[0] - y_regr[1] + y_regr[2] + y_regr[3] - y_regr[4] - y_regr[5] + y_regr[6] + y_regr[7]) / N,
-        (-y_regr[0] + y_regr[1] - y_regr[2] + y_regr[3] - y_regr[4] + y_regr[5] - y_regr[6] + y_regr[7]) / N,
-        (y_regr[0] + y_regr[1] - y_regr[2] - y_regr[3] - y_regr[4] - y_regr[5] + y_regr[6] + y_regr[7]) / N,
-        (y_regr[0] - y_regr[1] + y_regr[2] - y_regr[3] - y_regr[4] + y_regr[5] - y_regr[6] + y_regr[7]) / N,
-        (y_regr[0] - y_regr[1] - y_regr[2] + y_regr[3] + y_regr[4] - y_regr[5] - y_regr[6] + y_regr[7]) / N,
-        (-y_regr[0] + y_regr[1] + y_regr[2] - y_regr[3] + y_regr[4] - y_regr[5] - y_regr[6] + y_regr[7]) / N]
-t = [abs(i) / Sb for i in beta]
-t_kr = t_table[f3 - 1]
-print(t_kr)
-print(t)
-t_final = list(filter(lambda x: x > t_kr, t))
-f4 = N - len(t_final)
-print("\nЗначимі коефіцієнти: "), print_arr(t_final)
-print("t: "+str(t))
-print("t фінальна: "+str(t_final))
-print("Бета: "+str(beta))
-
-f_sum= 0
-for i in range(N):
-    f_sum+= pow((t[i] - y_regr[i]), 2)
-D_ad = (m / (f4)) *f_sum
-Fp = D_ad / Sb
-print("Fp = {}".format(round(Fp,5)))
-Ft = F_table[f3 - 1][f4 - 1]
-print("Ft = {}".format(round(Ft,5)))
-if Ft > Fp:
-    print(f"Ft > Fp\nРівняння регресії адекватно оригіналу при рівні значимості {q}")
-else:
-    print(f"Ft < Fp\nРівняння регресії неадекватно оригіналу при рівні значимості {q}")
-
 
